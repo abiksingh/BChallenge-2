36b21c3dfe273283f457eeccf03022a55a5ed228
Abhishek Singh <abi.k.singh64@gmail.com>
2021-10-16 13:25:42 +0200

    stylings left

--- src/components/LaunchScreen.tsx
+++ src/components/LaunchScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllLaunches, getLaunchById } from '../redux/actions/launchActions';
 import Spinner from '../UIHelpers/spinner';
@@ -9,7 +9,7 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-
+import { Grid, TextField } from '@mui/material';
 const LaunchScreen = () => {
   const dispatch = useDispatch();
 
@@ -29,42 +29,66 @@ const LaunchScreen = () => {
     dispatch(getAllLaunches());
   }, [dispatch]);
 
+  const [id, setId] = useState('');
+  console.log(id);
+  console.log(launchById);
+
+  const onClickHandler = () => {
+    dispatch(getLaunchById(id));
+  };
+
   return (
     <>
       <Header />
-      {loading ? (
-        <Spinner />
-      ) : (
-        allLaunch?.map((launch: any) => (
-          <Box key={launch.id} sx={{ minWidth: 275 }}>
-            <Card variant="outlined">
-              <CardContent>
-                <Typography
-                  sx={{ fontSize: 14 }}
-                  color="text.secondary"
-                  gutterBottom
-                >
-                  Word of the Day
-                </Typography>
-                <Typography variant="h5" component="div">
-                  benevolent
-                </Typography>
-                <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                  adjective
-                </Typography>
-                <Typography variant="body2">
-                  well meaning and kindly.
-                  <br />
-                  {'"a benevolent smile"'}
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button size="small">Learn More</Button>
-              </CardActions>
-            </Card>
-          </Box>
-        ))
-      )}
+      <Box
+        sx={{
+          width: 500,
+          maxWidth: '100%',
+        }}
+      >
+        <TextField
+          onChange={(e: any) => setId(e.target.value)}
+          fullWidth
+          label="fullWidth"
+          id="fullWidth"
+        />
+        <Button onClick={onClickHandler} variant="outlined">
+          Contained
+        </Button>
+      </Box>
+
+      <Grid container spacing={{ xs: 2, md: 2 }}>
+        {loading ? (
+          <Spinner />
+        ) : (
+          allLaunch?.map((launch: any) => (
+            <Grid
+              container
+              direction="column"
+              alignItems="center"
+              display="flex"
+              key={launch.id}
+              item
+              lg={4}
+              md={6}
+              sm={12}
+              xs={12}
+              // sx={albumStyle}
+            >
+              <Card variant="outlined">
+                <CardContent>
+                  <Typography variant="h5" component="div">
+                    {launch.name}
+                  </Typography>
+                  <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                    {launch.id}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))
+        )}
+      </Grid>
     </>
   );
 };
